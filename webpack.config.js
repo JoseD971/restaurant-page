2629e326212ca790b7e69670538baf339ac5fd3e
JoseD971 <157433709+JoseD971@users.noreply.github.com>
2024-07-09 11:44:34 -0500

    Configuración inicial del proyecto. Carpeta src con los archivos index.html e index.js y archivos json y js de configuración en el root
    .gitignore: Ignorar las carpetas dist y node_modules

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devtool: 'inline-source-map',
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff2|woff|ttf|otf|eot)$/i,
+        type: 'asset/resource',
+      },
+    ]
+  },
+};
